f57a743cd1156e7fb138945c9a12ab3bbd38f822
TicTac14 <francisco.mendez114@gmail.com>
2023-11-05 10:54:59 -0500

    g

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const taskRoutes = require('./routes/tasks.js')
+const taskRoutes = require('./routes/tasks.js');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // "/" route
-app.use(express.static('public'));
+app.use(express.static('public/Main Page'));
 // routes
 
 app.use('/api/v1/tasks', taskRoutes);
